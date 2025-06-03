02c890b168687d6cb7790aa6081adfa1e01ccdd2
Laxmikant Dubey <132117545+Laxmikant3@users.noreply.github.com>
2025-06-03 22:35:56 +0530

    Time: 0 ms (100%), Space: 44 MB (62.81%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.cpp
+++ 0888-fair-candy-swap/0888-fair-candy-swap.cpp
@@ -1,25 +1,18 @@
 class Solution {
-    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
-        
-        int aliceSum = 0, bobSum = 0;
-
-        for (int i = 0; i < aliceSizes.length; i++) {
-            aliceSum += aliceSizes[i];
+public:
+    vector<int> fairCandySwap(vector<int>& A, vector<int>& B) {
+        bitset<200002> bf;
+        int sumA = 0, sumB = 0;
+        for (auto n : A) sumA += n;
+        for (auto n : B) {
+            sumB += n;
+            bf.set(2 * n);
         }
-
-        for (int i = 0; i < bobSizes.length; i++) {
-            bobSum += bobSizes[i];
+        int diff = sumA - sumB;
+        for (auto n : A) {
+            int det = 2 * n - diff;
+            if (det > 0 && det < 200002 && bf.test(det)) return {n, (2 * n - diff) / 2};
         }
-
-        for (int i = 0; i < aliceSizes.length; i++) {
-            for (int j = 0; j < bobSizes.length; j++) {
-                if ((aliceSum - aliceSizes[i] + bobSizes[j]) == 
-                    (bobSum - bobSizes[j] + aliceSizes[i])) {
-                    return new int[] { aliceSizes[i], bobSizes[j] };
-                }
-            }
-        }
-
-        return new int[] {};
+        return {};
     }
-}
+};
