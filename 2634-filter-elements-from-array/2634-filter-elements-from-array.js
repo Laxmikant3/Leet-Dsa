2613892a1dfdce46ed67d91f737965a8c699a737
Laxmikant Dubey <132117545+Laxmikant3@users.noreply.github.com>
2026-01-03 22:32:56 +0530

    Time: 42 ms (75.45%), Space: 53.7 MB (70.3%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    var out = [];
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+            out.push(arr[i]);
+        }
+    }
+    return out;
+};
