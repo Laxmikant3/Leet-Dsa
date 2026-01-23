e5c1d6bd8e53d00bfd71d669fae0678e953afaad
Laxmikant Dubey <132117545+Laxmikant3@users.noreply.github.com>
2026-01-23 23:40:28 +0530

    Time: 0 ms (100%), Space: 26.5 MB (61.84%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -1,33 +1,31 @@
 class Solution {
-  int minimumPairRemoval(List<int> nums) {
-    int operations = 0;
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        int operations = 0;
 
-    while (!_isNonDecreasing(nums)) {
-      int minSum = nums[0] + nums[1];
-      int index = 0;
+        while (!isNonDecreasing(nums)) {
+            int minSum = nums[0] + nums[1];
+            int index = 0;
 
-      // Find leftmost adjacent pair with minimum sum
-      for (int i = 1; i < nums.length - 1; i++) {
-        int sum = nums[i] + nums[i + 1];
-        if (sum < minSum) {
-          minSum = sum;
-          index = i;
+            for (int i = 1; i < nums.size() - 1; i++) {
+                int sum = nums[i] + nums[i + 1];
+                if (sum < minSum) {
+                    minSum = sum;
+                    index = i;
+                }
+            }
+
+            nums[index] = minSum;
+            nums.erase(nums.begin() + index + 1);
+            operations++;
         }
-      }
-
-      // Merge the chosen pair
-      nums[index] = minSum;
-      nums.removeAt(index + 1);
-      operations++;
+        return operations;
     }
 
-    return operations;
-  }
-
-  bool _isNonDecreasing(List<int> nums) {
-    for (int i = 1; i < nums.length; i++) {
-      if (nums[i] < nums[i - 1]) return false;
+    bool isNonDecreasing(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) return false;
+        }
+        return true;
     }
-    return true;
-  }
-}
+};
